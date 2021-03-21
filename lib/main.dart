8b80513550b82a43c4aba332d5007c5c8c6f1413
catarina-21805632 <catarina.mlmoita@gmail.com>
2021-03-21 17:37:35 +0000

    GRÁFICO RESOLVIDO ALELUIA

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_tfc/eventsToApprove.dart';
-import 'package:fcharts/fcharts.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:projeto_tfc/times.dart';
 import 'events.dart';
 import 'memberToApprove.dart';
@@ -799,64 +799,87 @@ class _MembersListScreenState extends State<MembersListScreen>{
   }
 }
 
-class SimpleLineChart extends StatelessWidget {
-  // X value -> Y value
-  static const myData = [
-    ["A", "✔"],
-    ["B", "❓"],
-    ["C", "✖"],
-    ["D", "❓"],
-    ["E", "✖"],
-    ["F", "✖"],
-    ["G", "✔"],
-  ];
+class SimpleTimeSeriesChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  SimpleTimeSeriesChart(this.seriesList, {this.animate});
+
+  /// Creates a [TimeSeriesChart] with sample data and no transition.
+  factory SimpleTimeSeriesChart.withSampleData() {
+    return new SimpleTimeSeriesChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 4 / 3,
-      child: new LineChart(
-        lines: [
-          new Line<List<String>, String, String>(
-            data: myData,
-            xFn: (datum) => datum[0],
-            yFn: (datum) => datum[1],
-          ),
-        ],
-        chartPadding: new EdgeInsets.fromLTRB(30.0, 10.0, 10.0, 30.0),
-      ),
+    return new charts.TimeSeriesChart(
+      seriesList,
+      animate: animate,
+      // Optionally pass in a [DateTimeFactory] used by the chart. The factory
+      // should create the same type of [DateTime] as the data provided. If none
+      // specified, the default creates local date time.
+      dateTimeFactory: const charts.LocalDateTimeFactory(),
     );
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData() {
+    final data = [
+      new TimeSeriesSales(new DateTime(2017, 9, 19), 1.50),
+      new TimeSeriesSales(new DateTime(2017, 9, 26),  3.50),
+      new TimeSeriesSales(new DateTime(2017, 10, 3),  2.33),
+      new TimeSeriesSales(new DateTime(2017, 10, 10),  5.01),
+    ];
+
+    return [
+      new charts.Series<TimeSeriesSales, DateTime>(
+        id: 'Times',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (TimeSeriesSales sales, _) => sales.date,
+        measureFn: (TimeSeriesSales sales, _) => sales.time,
+        data: data,
+      )
+    ];
   }
 }
 
-class ChartExample {
-  ChartExample(
-      this.name,
-      this.widget,
-      this.description,
-      );
+/// Sample time series data type.
+class TimeSeriesSales {
+  final DateTime date;
+  final double time;
 
-  final String name;
-  final Widget widget;
-  final String description;
+  TimeSeriesSales(this.date, this.time);
 }
-
-final charts = [
-  new ChartExample(
-    'Simple Line Chart',
-    new SimpleLineChart(),
-    'Strings on the X-Axis and their index in the list on the Y-Axis.',
-  ),
-];
 
 class _StatsScreenState extends State<StatsScreen>{
 
-  var _chartIndex = 0;
+  static List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData() {
+    final data = [
+      new TimeSeriesSales(new DateTime(2017, 9, 19), 1.50),
+      new TimeSeriesSales(new DateTime(2017, 9, 26),  3.50),
+      new TimeSeriesSales(new DateTime(2017, 10, 3),  2.33),
+      new TimeSeriesSales(new DateTime(2017, 10, 10),  5.01),
+    ];
+
+
+    return [
+      new charts.Series<TimeSeriesSales, DateTime>(
+        id: 'Times',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (TimeSeriesSales sales, _) => sales.date,
+        measureFn: (TimeSeriesSales sales, _) => sales.time,
+        data: data,
+      )
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    final chart = charts[_chartIndex % charts.length];
 
     return Scaffold(
         appBar: AppBar(
@@ -880,8 +903,17 @@ class _StatsScreenState extends State<StatsScreen>{
                           color: Colors.black,
                         ),),
                       ),
-
-                      SizedBox(height: 40),
+                      Padding(
+                        padding: new EdgeInsets.all(32.0),
+                        child: new SizedBox(
+                          height: 200.0,
+                          width: 340,
+                          child: new charts.TimeSeriesChart(
+                            _createSampleData(),
+                              animate: true,
+                          ),
+                        ),
+                      ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
