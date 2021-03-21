a522f5151131f5e5fc8b3e787d72707d2026e132
catarina-21805632 <catarina.mlmoita@gmail.com>
2021-03-21 16:12:42 +0000

    detalhes e inscrição em eventos mas ainda sem verifiações previas

--- lib/events.dart
+++ lib/events.dart
@@ -2,31 +2,52 @@ class Events {
 
   String name;
   String date;
+  String deadline;
+  String local;
+  String type;
+  String cost;
 
-  Events(this.name, this.date);
 
   Events.fromJson(Map<String, dynamic> json)
       : name = json['name'].toString(),
-        date = json['date'].toString();
+        date = json['date'].toString(),
+        deadline = json['dealine'].toString(),
+        local = json['local'].toString(),
+        type = json['type'].toString(),
+        cost = json['cost'].toString();
 
   static final getEvents = [
     {
       'name': 'Corrida 25 de Abril',
       'date': '25/04/2021',
+      'deadline': '1/03/2021',
+      'local': 'Lisboa',
+      'type': '10km',
+      'cost': '5€',
       'image': 'assets/images/25abril.jpg',
 
     },
     {
       'name': 'Maratona EDP',
       'date': '10/05/2021',
+      'deadline': '15/04/2021',
+      'local': 'Lisboa',
+      'type': 'Maratona',
+      'cost': '10€',
       'image': 'assets/images/edp.jpg',
 
     },
     {
       'name': 'São Silvestre Amadora',
       'date': '31/12/2021',
+      'deadline': '1/12/2021',
+      'local': 'Amadora',
+      'type': '10km',
+      'cost': '7€',
       'image': 'assets/images/saosilvestre.jpg',
 
     },
   ];
+
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -100,17 +100,186 @@ class _DetailsScreenState extends State<DetailsScreen>{
   Widget build(BuildContext context) {
 
     return Scaffold(
-        appBar: AppBar(
-          iconTheme: IconThemeData(color: Colors.white),
-          title: Text(
-              event
-          ),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          titulo(event),
         ),
-        body: Text(
-          event
-        ),
+      ),
+      body: new LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: Container(
+                  margin: const EdgeInsets.all(20.0),
+                  child: Column(
+                      children: <Widget>[
+                        Center(
+                          child: Image(image: AssetImage(imagem(event)), width: 200,
+                            height: 150,),
+                        ),
+                        SizedBox(height: 50),
+                        Text(
+                            'Date: ' + data(event)
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                            'Deadline: ' + prazo(event)
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                            'Local: ' + local(event)
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                            'Type: ' + tipo(event)
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                            'Cost: ' + custo(event)
+                        ),
+                        SizedBox(height: 50),
+                        ButtonTheme(
+                          minWidth: 380.0,
+                          height: 55.0,
+                          child: RaisedButton(
+                            child: Text(
+                              'Register',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20
+                              ),
+                            ),
+                            onPressed: () {
+                              showAlertDialog(context);
+                            },
+                          ),
+                        ),
+                      ]
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
     );
   }
+
+  void showAlertDialog(BuildContext context) {
+
+    // set up the buttons
+    Widget cancelButton = FlatButton(
+      child: Text("Cancel"),
+      onPressed:  () {
+        Navigator.pop(context);
+      },
+    );
+    Widget continueButton = RaisedButton(
+      child: Text("Register"),
+      color: Colors.green,
+      onPressed:  () {
+        Navigator.pop(context);
+        showAlertDialogSucesso(context);
+      },
+    );
+
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text(titulo(event)),
+      content: Text("Are you sure you want to register for this event?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  void showAlertDialogSucesso(BuildContext context) {
+
+    Widget okButton = FlatButton(
+      child: Text("OK"),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text('Successfully Registered'),
+      content: Text("You are registered in ${titulo(event)}."),
+      actions: [
+        okButton,
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  String titulo(String event){
+    var arr = event.split(",");
+    var arr2 = arr[0].split(": ");
+
+    return arr2[1];
+  }
+
+  String data(String event){
+    var arr = event.split(",");
+    var arr2 = arr[1].split(": ");
+
+    return arr2[1];
+  }
+
+  String prazo(String event){
+    var arr = event.split(",");
+    var arr2 = arr[2].split(": ");
+
+    return arr2[1];
+  }
+
+  String local(String event){
+    var arr = event.split(",");
+    var arr2 = arr[3].split(": ");
+
+    return arr2[1];
+  }
+
+  String tipo(String event){
+    var arr = event.split(",");
+    var arr2 = arr[4].split(": ");
+
+    return arr2[1];
+  }
+
+  String custo(String event){
+    var arr = event.split(",");
+    var arr2 = arr[5].split(": ");
+
+    return arr2[1];
+  }
+
+  String imagem(String event){
+    var arr = event.split(",");
+    var arr2 = arr[6].split(": ");
+
+    return arr2[1].replaceAll('}', '');
+  }
+
 }
 
 class _HomeScreenState extends State<HomeScreen>{
@@ -176,19 +345,19 @@ class _HomeScreenState extends State<HomeScreen>{
                         builder: (context) => DetailsScreen(Events.getEvents[index].toString()),
                       ));
                 },
-              child: Container(
-                padding: EdgeInsets.fromLTRB(15,15,15,0),
-                height: 200,
-                child: Card(
-                  elevation: 7,
-                  child: Row(
-                    children: [
-                      image(Events.getEvents[index]),
-                      event(Events.getEvents[index])
-                    ],
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(15,15,15,0),
+                  height: 200,
+                  child: Card(
+                    elevation: 7,
+                    child: Row(
+                      children: [
+                        image(Events.getEvents[index]),
+                        event(Events.getEvents[index])
+                      ],
+                    ),
                   ),
-                ),
-              )
+                )
 
             );
           }),
@@ -211,8 +380,8 @@ class _HomeScreenState extends State<HomeScreen>{
                         Padding(padding: EdgeInsets.all(15.0),
                           child: Text(
                             'John Doe',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        ),
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
                         )
                       ]
                   ),
@@ -286,9 +455,8 @@ class _HomeScreenState extends State<HomeScreen>{
                 ),
               ),
               onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
+                    LoginScreen()), (Route<dynamic> route) => false
                 );
               },
             ),
@@ -306,146 +474,146 @@ class _LoginScreenState extends State<LoginScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:
-        SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                RichText(
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(text: 'Sign In', style: TextStyle(color: Colors.grey[850], height: 5, fontSize: 40, fontFamily: 'Arial')),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 40),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(padding: EdgeInsets.only(left: 20),
-                    child: Text(
-                      'Username',
-                      style: TextStyle(
-                        color: Colors.cyan,
-                        fontSize: 17,
-                        fontFamily: 'Arial',
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  height: 60,
-                  width: 380,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.only(top: 14.0),
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Colors.cyan,
-                      ),
-                      hintText: 'Enter Username',
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(padding: EdgeInsets.only(left: 20),
-                    child: Text(
-                      'Password',
-                      style: TextStyle(
-                        color: Colors.cyan,
-                        fontSize: 17,
-                        fontFamily: 'Arial',
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  height: 60,
-                  width: 380,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.only(top: 14.0),
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Colors.cyan,
-                      ),
-                      hintText: '**********',
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                padding: EdgeInsets.only(left: 5),
-                  child: Row(
-                    children: <Widget>[
-                      Theme(
-                        data: ThemeData(unselectedWidgetColor: Colors.black),
-                        child: Checkbox(
-                          value: _rememberMe,
-                          checkColor: Colors.white,
-                          activeColor: Colors.cyan,
-                          onChanged: (value) {
-                            setState(() {
-                              _rememberMe = value;
-                            });
-                          },
-                        ),
-                      ),
-                      Text(
-                        'Remember me',
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 30),
-                ButtonTheme(
-                  minWidth: 380,
-                  child: RaisedButton(
-                    color: Colors.cyan,
-                    onPressed: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeScreen(title: 'Sistema de Gestão de Eventos')),
-                      );
-                    },
-                    padding: EdgeInsets.all(15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+      body:
+      SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(text: 'Sign In', style: TextStyle(color: Colors.grey[850], height: 5, fontSize: 40, fontFamily: 'Arial')),
+                ],
+              ),
             ),
+            SizedBox(height: 40),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Username',
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 17,
+                    fontFamily: 'Arial',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              alignment: Alignment.centerLeft,
+              height: 60,
+              width: 380,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.black),
+              ),
+              child: TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 14.0),
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: Colors.cyan,
+                  ),
+                  hintText: 'Enter Username',
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Password',
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 17,
+                    fontFamily: 'Arial',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              alignment: Alignment.centerLeft,
+              height: 60,
+              width: 380,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.black),
+              ),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 14.0),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.cyan,
+                  ),
+                  hintText: '**********',
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.only(left: 5),
+              child: Row(
+                children: <Widget>[
+                  Theme(
+                    data: ThemeData(unselectedWidgetColor: Colors.black),
+                    child: Checkbox(
+                      value: _rememberMe,
+                      checkColor: Colors.white,
+                      activeColor: Colors.cyan,
+                      onChanged: (value) {
+                        setState(() {
+                          _rememberMe = value;
+                        });
+                      },
+                    ),
+                  ),
+                  Text(
+                    'Remember me',
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            ButtonTheme(
+              minWidth: 380,
+              child: RaisedButton(
+                color: Colors.cyan,
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen(title: 'Sistema de Gestão de Eventos')),
+                  );
+                },
+                padding: EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  'LOGIN',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
@@ -585,10 +753,10 @@ class _MembersListScreenState extends State<MembersListScreen>{
                 elevation: 7,
                 child: Column(
                   children: <Widget>[
-                Align(
-                alignment: Alignment.topLeft,
-                  child: memberToApprove(MembersToAprrove.getMember[index]),
-                ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: memberToApprove(MembersToAprrove.getMember[index]),
+                    ),
                     ButtonBar(
                       alignment: MainAxisAlignment.center,
                       children: <Widget>[
@@ -690,10 +858,10 @@ class _StatsScreenState extends State<StatsScreen>{
     final chart = charts[_chartIndex % charts.length];
 
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.white),
-        title: Text('Statistics'),
-      ),
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.white),
+          title: Text('Statistics'),
+        ),
         body: Container(
           child: Center(
             child: Padding(
@@ -712,10 +880,10 @@ class _StatsScreenState extends State<StatsScreen>{
                         ),),
                       ),
                       SizedBox(height: 35),
-                     Padding(
-                       padding: new EdgeInsets.all(20.0),
-                       child: chart.widget,
-                     ),
+                      Padding(
+                        padding: new EdgeInsets.all(20.0),
+                        child: chart.widget,
+                      ),
                       SizedBox(height: 40),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
